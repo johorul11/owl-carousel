9764bb068198a522820e44f391d0eaba872e2403
Md. Johorul Islam <67358128+johorul11@users.noreply.github.com>
2020-07-11 12:33:46 +0600

    Add files via upload

---
+++ owl carsol/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    loop:true,
+    margin:15,
+    responsiveClass:true,
+    responsive:{
+      0:{
+          items:3,
+          nav:true
+      },
+      620:{
+            items:4,
+            nav:true
+      },
+      1000:{
+             items:5,
+             nav:true
+      }
+    }
+  });
+});
+ 
